4f52306ebcf967b3fd3931dd288272acb39e08b0
jieunhanme <101976899+jieunhanme@users.noreply.github.com>
2022-04-17 22:59:24 +0900

    Feat : react-query 적용 (#5)
    Co-authored-by: jieun.han <jieun.han@brique.co.kr>

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,4 +1,27 @@
+import { useQuery } from "react-query";
+
 const HomePage = () => {
-  return <div>HOMEPAGE</div>;
+  const { status, error, data } = useQuery(
+    ["home"],
+    async () =>
+      await fetch("https://swapi.dev/api/people/").then((res) => res.json())
+  );
+
+  if (status === "loading") return <div>LOADING...</div>;
+  if (status === "error") return <div>ERROR :(</div>;
+
+  return (
+    <div>
+      {data.results.map((person: any) => {
+        const personUrlParts = person.url.split("/").filter(Boolean);
+        const personId = personUrlParts[personUrlParts.length - 1];
+        return (
+          <article key={personId} style={{ margin: "16px 0 0" }}>
+            {person.name}
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 export default HomePage;
